1cb6585f1b1013026acdb72c65751935278c7d85
Cady Xu <cadyxu@Cadys-MacBook-Pro.local>
2016-02-05 16:38:48 -0800

    completed

--- CrunchTime/app/src/main/java/com/cs160/cadyxu/crunchtime/MainActivity.java
+++ CrunchTime/app/src/main/java/com/cs160/cadyxu/crunchtime/MainActivity.java
@@ -1,11 +1,13 @@
 package com.cs160.cadyxu.crunchtime;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -29,10 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
         Spinner spinner = (Spinner) findViewById(R.id.beforeSpinner);
 
-        input = (EditText) findViewById(R.id.inputEditText);
+
         beforeSpinner = (Spinner)findViewById(R.id.beforeSpinner);
-        adapter = ArrayAdapter.createFromResource(this,R.array.units,android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter = ArrayAdapter.createFromResource(this,R.array.units,R.layout.spinner_layout);
+        adapter.setDropDownViewResource(R.layout.spinner_layout);
+
         beforeSpinner.setAdapter(adapter);
 
         afterSpinner = (Spinner) findViewById(R.id.afterSpinner);
@@ -43,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 beforeUnit = parent.getItemAtPosition(position).toString();
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
@@ -55,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 afterUnit = parent.getItemAtPosition(position).toString();
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
@@ -65,9 +67,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void convert(View v){
-        double value = new Double (input.getText().toString());
-        value = unitConverter.exerciseConvert(value, beforeUnit, afterUnit);
-        result.setText(new Double(value).toString());
+        input = (EditText) findViewById(R.id.inputEditText);
+        result = (EditText) findViewById(R.id.resultEditText);
+        if (input.getText().toString().length() == 0){
+            result.setText("0");
+        } else {
+            double value = new Double (input.getText().toString());
+            value = unitConverter.exerciseConvert(value, beforeUnit, afterUnit);
+
+            result.setText(new Double(value).toString());
+        }
+
 
     }
 
--- CrunchTime/app/src/main/java/com/cs160/cadyxu/crunchtime/unitConverter.java
+++ CrunchTime/app/src/main/java/com/cs160/cadyxu/crunchtime/unitConverter.java
@@ -12,24 +12,86 @@ public class unitConverter {
         double multiplier = 1;
         double divisor = 1;
 
-        switch (beforeType.toLowerCase()){
-            case "calories":
+        switch (beforeType){
+            case "Calories":
                 divisor = 100;
-            case "pushup*":
+                break;
+            case "Pushups (Reps)":
                 divisor = 350;
                 break;
-            case "situp*":
+            case "Situps (Reps)":
                 divisor = 200;
                 break;
+            case "Jumping Jacks (Mins)":
+                divisor = 10;
+                break;
+            case "Jogging (Mins)":
+                divisor = 12;
+                break;
+            case "Squats (Reps)":
+                divisor = 225;
+                break;
+            case "Leg-lift (Mins)":
+                divisor = 25;
+                break;
+            case "Plank (Mins)":
+                divisor = 25;
+                break;
+            case "Pullup (Reps)":
+                divisor = 100;
+                break;
+            case "Cycling (Mins)":
+                divisor = 12;
+                break;
+            case "Walking (Mins)":
+                divisor = 20;
+                break;
+            case "Swimming (Mins)":
+                divisor = 13;
+                break;
+            case "Stair-climbing (Mins)":
+                divisor = 15;
+                break;
         }
-        switch (afterType.toLowerCase()){
-            case "calories":
+        switch (afterType){
+            case "Calories":
                 multiplier = 100;
-            case "pushup*":
+                break;
+            case "Pushups (Reps)":
                 multiplier = 350;
                 break;
-            case "situp*":
+            case "Situps (Reps)":
                 multiplier = 200;
+                break;
+            case "Jumping Jacks (Mins)":
+                multiplier = 10;
+                break;
+            case "Jogging (Mins)":
+                multiplier = 12;
+                break;
+            case "Squats (Reps)":
+                multiplier = 225;
+                break;
+            case "Leg-lift (Mins)":
+                multiplier = 25;
+                break;
+            case "Plank (Mins)":
+                multiplier = 25;
+                break;
+            case "Pullup (Reps)":
+                multiplier = 100;
+                break;
+            case "Cycling (Mins)":
+                multiplier = 12;
+                break;
+            case "Walking (Mins)":
+                multiplier = 20;
+                break;
+            case "Swimming (Mins)":
+                multiplier = 13;
+                break;
+            case "Stair-climbing (Mins)":
+                multiplier = 15;
                 break;
         }
         return number * multiplier / divisor;
